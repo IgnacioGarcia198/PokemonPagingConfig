1d0086a27613af65c1b145d71ca4d53d6e52debb
Ignacio Garcia <ignaciogarcia198@gmail.com>
2019-10-23 03:25:35 +0900

    I managed to relaunch the download when we recover the network after we didn't have it for a while. The download is from the beginning, from the pokemon number 1. This means that if we lose the connection when we are downloading a further part of the list, on recovering the connection the RecyclerView will go back to the top, first position.

--- app/src/main/java/com/ignacio/pokemonpagingconfig/api/PokemonDownloadWorker.java
+++ app/src/main/java/com/ignacio/pokemonpagingconfig/api/PokemonDownloadWorker.java
@@ -1,8 +1,10 @@
 package com.ignacio.pokemonpagingconfig.api;
 
+import android.app.ActivityManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -25,15 +27,19 @@ import com.bumptech.glide.request.transition.Transition;
 import com.ignacio.pokemonpagingconfig.MainActivity;
 import com.ignacio.pokemonpagingconfig.R;
 import com.ignacio.pokemonpagingconfig.data.RetroPokemonRepository;
+import com.ignacio.pokemonpagingconfig.utils.PepeEvent;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class PokemonDownloadWorker extends Worker {
     private static final String WORK_RESULT = "work_result";
     //private NotificationManager notificationManager;
-    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
-    public static final int NOTIFICATION_ID = 0;
-    NotificationManager manager;
+
+    //NotificationManager manager;
 
 
     public PokemonDownloadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
@@ -43,94 +49,18 @@ public class PokemonDownloadWorker extends Worker {
     @NonNull
     @Override
     public ListenableWorker.Result doWork() {
-        String name = getInputData().getString("name");
-        RetroPokemonRepository.getInstance(getApplicationContext()).searchPokemons(name);
-        //Data taskData = getInputData();
-        //String taskDataString = taskData.getString(MainActivity.MESSAGE_STATUS);
-        //runTheJob();
-        //showNotification("WorkManager", taskDataString != null ? taskDataString : "Message has been Sent");
+        //String name = getInputData().getString("name");
+        if(MainActivity.activityIsActive) {
+            // do the job now!!! with an interface we
+            EventBus.getDefault().post(new PepeEvent(true));
+        }
+        //RetroPokemonRepository.getInstance(getApplicationContext()).searchPokemons(name);
+
 
         Data outputData = new Data.Builder().putString(WORK_RESULT, "Jobs Finished").build();
 
         return Result.success(outputData);
 
     }
-
-    private void showNotification(Bitmap resource) {
-
-        manager = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
-
-        createNotificationChannel();
-        Intent contentIntent = new Intent(getApplicationContext(), MainActivity.class);
-        //contentIntent.putExtra("id", pokId);
-        PendingIntent contentPendingIntent = PendingIntent.getActivity(
-                getApplicationContext(),NOTIFICATION_ID,contentIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                getApplicationContext(),PRIMARY_CHANNEL_ID)
-                .setStyle(new NotificationCompat.BigPictureStyle()
-                                .bigPicture(resource)//.bigLargeIcon(resource)
-                        /*.setBigContentTitle("Notification Updated!")*/)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setColor(Color.YELLOW)
-                .setLargeIcon(resource)
-                .setContentTitle("Job is finished!")
-                .setContentText("Job finished execution")
-                .setContentIntent(contentPendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL);
-
-        manager.notify(NOTIFICATION_ID,builder.build());
-        //Log.d(LOG_TAG,"Job finished successfully");
-        //jobFinished(params,false);
-        //jobScheduler.cancel(MainActivity.INITIAL_JOB_ID);
-        //jobScheduler =(JobScheduler)getSystemService(JOB_SCHEDULER_SERVICE);
-
-    }
-
-
-    private void runTheJob() {
-        // TODO IMPLEMENT THE REAL TASK OF THE WORKER.
-        //MainActivity.scheduleDelayedWork(2*60*1000,getApplicationContext());
-        String path = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/1.png";
-        RequestOptions options = new RequestOptions()
-                .centerCrop()
-                //.fitCenter()
-                //.placeholder(R.mipmap.ic_launcher_round)
-                .error(R.mipmap.ic_launcher_round);
-        Glide.with(getApplicationContext())
-                .asBitmap()
-                .load(path)
-                //.dontAnimate()
-                .apply(options)
-                .into(new CustomTarget<Bitmap>(475,475) {
-                    @Override
-                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                        showNotification(resource);
-
-                    }
-
-                    @Override
-                    public void onLoadCleared(@Nullable Drawable placeholder) {
-
-                    }
-                });
-    }
-
-    public void createNotificationChannel() {
-        manager = (NotificationManager)getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel notificationChannel= new NotificationChannel(
-                    PRIMARY_CHANNEL_ID,"Test Notifications", NotificationManager.IMPORTANCE_HIGH);
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.RED);
-            notificationChannel.enableVibration(true);
-            notificationChannel.setDescription("notifications for a job test app");
-            manager.createNotificationChannel(notificationChannel);
-        }
-    }
-
 
 }
---
+++ app/src/main/java/com/ignacio/pokemonpagingconfig/utils/PepeEvent.java
@@ -0,0 +1,8 @@
+package com.ignacio.pokemonpagingconfig.utils;
+
+public class PepeEvent {
+    public final boolean flag;
+    public PepeEvent(boolean flag) {
+        this.flag = flag;
+    }
+}
